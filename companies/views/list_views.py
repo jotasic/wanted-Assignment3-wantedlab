17d7cd41a5cbb28156c138b1ec1720d7ebe6ba9a
TaewooKim <embedded61@gmail.com>
2021-11-10 02:04:33 +0900

    Hotfix: query string이 없을 때 서버가 다운되는 현상 수정

--- companies/views/list_views.py
+++ companies/views/list_views.py
@@ -8,23 +8,21 @@ from django.core.exceptions                   import ObjectDoesNotExist
 class CompanySearchAPIView(views.APIView):
 
     def get(self, request):
-        try:    
-            search = request.GET.get('query', None)
-            if request.headers['x-wanted-language'] is False:
-                language = 'ko'
-            else :
-                language = request.headers['x-wanted-language']
+        search  = request.GET.get('query', None)
+        results = []
 
-            c = {f'company_name__{language}__icontains': search}
-
-            companies = Company.objects.filter(**c)
-            company_serializer = CompanySerializer(companies, many=True)
-
-            results = [{
-                "company_name" : company['company_name'][language]
-            } for company in company_serializer.data]
-
+        if search is None:
             return response.Response(results, status=status.HTTP_200_OK)
-        
-        except ObjectDoesNotExist:
-            return response.Response(results, status=status.HTTP_404_NOT_FOUND)
+
+        language = request.headers.get('x-wanted-language', 'ko')
+
+        c = {f'company_name__{language}__icontains': search}
+
+        companies = Company.objects.filter(**c)
+        company_serializer = CompanySerializer(companies, many=True)
+
+        results = [{
+             "company_name" : company['company_name'][language]
+        } for company in company_serializer.data]
+
+        return response.Response(results, status=status.HTTP_200_OK)
